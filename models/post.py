e8683d60c32315704fa1b3298f3c8b651dc4d635
rubengura <ruben.guerrero.ramirez@gmail.com>
2019-12-09 23:06:29 +0100

    blog and post files completed

--- models/post.py
+++ models/post.py
@@ -29,6 +29,18 @@ class Post:
             'created_date': self.created_date
         }
 
-    #@staticmethod
-    #def from_mongo(id):
-    #    data = Database.find_one(collection='posts', query={'id'=id})
+    @classmethod
+    def from_mongo(cls, id):
+        post_data = Database.find_one(collection='posts',
+                                      query={'id': id})
+        return cls(blog_id=post_data['blog_id'],
+                   title=post_data['title'],
+                   content=post_data['content'],
+                   author=post_data['author'],
+                   date=post_data['created_date'],
+                   id=post_data['id'])
+
+    @staticmethod
+    def from_blog(id):
+        return [post for post in Database.find(collection='posts', query={'blog_id': id})]
+
